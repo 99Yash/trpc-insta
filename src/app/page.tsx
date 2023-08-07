913bf6b038db1d6f5dbf8fbf68d54f583e9c81b9
yash <yashgouravkar@gmail.com>
2023-08-07 12:15:58 +0530

    changes to post page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,41 +13,24 @@ export default async function Index() {
       },
       include: {
         images: true,
-        comments: {
-          take: 1,
-          include: {
-            user: {
-              select: {
-                username: true,
-                image: true,
-                name: true, //? for fallback image
-              },
-            },
-          },
-        },
+        user: true,
       },
     });
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center gap-6 mb-2">
         {randomPosts.map((post) => (
-          <div
-            key={post.id}
-            className="flex border border-gray-500 flex-col gap-3 items-center "
-          >
+          <div key={post.id} className="flex flex-col gap-3">
             <Image
               src={post.images[0]?.url as string}
-              alt="Cant preview image"
-              width={400}
-              height={400}
+              alt={post.caption || 'Cant preview image'}
+              width={590}
+              height={590}
             />
-            <div className="flex gap-2">
-              <CustomAvatar
-                imgUrl={post.comments[0]?.user.image as string}
-                name={post?.comments[0]?.user.name as string}
-              />
-              <p>{post.comments[0]?.user.username}</p>
-              <p>{post.comments[0]?.text}</p>
+            <div className="flex flex-wrap items-center gap-1 px-2">
+              <p>{post?.user.username}</p>
+              <p className="text-sm">{post?.caption}</p>
             </div>
+            <hr className="border-0 h-px mt-2 bg-slate-700" />
           </div>
         ))}
       </div>
@@ -56,6 +39,9 @@ export default async function Index() {
   const feedPosts = await prisma.post.findMany({
     where: {
       userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
     },
     include: {
       images: true,
@@ -66,49 +52,31 @@ export default async function Index() {
           name: true,
         },
       },
-      comments: {
-        include: {
-          user: {
-            select: {
-              username: true,
-              image: true,
-              name: true,
-            },
-          },
-        },
-      },
     },
     take: 5,
   });
   return (
-    <div className="flex flex-col gap-3 items-center mb-2 ">
+    <div className="flex flex-col gap-6 items-center mb-2 ">
       {feedPosts.map((post) => (
-        <div
-          key={post.id}
-          className="flex border border-gray-500 flex-col gap-3"
-        >
-          <Image
-            src={post.images[0]?.url as string}
-            alt={post.caption || 'Cant preview image'}
-            width={400}
-            height={400}
-          />
-          <div className="flex flex-wrap items-center gap-1 px-2">
+        <div key={post.id} className="flex flex-col gap-3">
+          <div className="flex items-center">
             <CustomAvatar
               imgUrl={post.user.image as string}
               name={post?.user.name as string}
             />
             <p>{post?.user.username}</p>
+          </div>
+          <Image
+            src={post.images[0]?.url as string}
+            alt={post.caption || 'Cant preview image'}
+            width={640}
+            height={640}
+          />
+          <div className="flex flex-wrap items-center gap-1 px-2">
+            <p>{post?.user.username}</p>
             <p className="text-sm">{post?.caption}</p>
           </div>
-          <div className="flex flex-wrap items-center gap-1 px-6">
-            <CustomAvatar
-              imgUrl={post.comments[0]?.user.image as string}
-              name={post?.comments[0]?.user.name as string}
-            />
-            <p>{post.comments[0]?.user.username}</p>
-            <p className="text-sm">{post.comments[0]?.text}</p>
-          </div>
+          <hr className="border-0 h-px mt-2 bg-slate-700" />
         </div>
       ))}
     </div>
