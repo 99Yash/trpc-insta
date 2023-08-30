36cee7ce76d3a0cdc4b5cf02a18ad51268d144d2
yash <yashgouravkar@gmail.com>
2023-08-30 16:22:22 +0530

    feat: adds proxy db url

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    DIRECT_URL: z.string().url(),
     NODE_ENV: z.enum(['development', 'test', 'production']),
     // NEXTAUTH_SECRET:
     //   process.env.NODE_ENV === 'production'
@@ -39,6 +40,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
